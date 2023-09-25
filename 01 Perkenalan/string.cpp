c7ddb7882958f81595d198e1b4e54ea3db0e86d7
unknown <tamagala606@gmail.com>
2023-09-25 22:54:44 +0700

    Tugas 2 DDP

--- 01 Perkenalan/string.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    string umur, nama, mati;
-    cin >> umur >> nama >> mati;
-
-    cout << umur << nama << mati << endl;
-}
--- 01 Perkenalan/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int value1, value2;
-    cin >> value1, value2;
-
-    cout << "Hasil dari Penjumlahan " << value1 << " dengan " << value2 << " adalah " << value1 + value2 << endl;
-}
